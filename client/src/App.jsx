f3965601e8929c8bb116d3599f6d4c3c1b16ed58
DanailDanov <114019922+DanailDanov@users.noreply.github.com>
2023-11-11 16:10:49 +0200

    adding react router dom

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,12 @@
 
 
 function App() {
- <div>
-    <h1>Danail</h1>
- </div>
+    return (
+        <div>
+            <h1>Danail</h1>
+            <p>skljdkjd</p>
+        </div>
+    )
 }
 
 export default App
